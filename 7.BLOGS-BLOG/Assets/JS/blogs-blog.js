28c93967f668f7577095ceb23e1d02b82ce27671
Zakir Rahimli <60932492+RahimliZakir@users.noreply.github.com>
2020-07-18 07:13:44 +0400

    Add files via upload

---
+++ 7.BLOGS-BLOG/Assets/JS/blogs-blog.js
@@ -0,0 +1,110 @@
+$(document).ready(function () {
+   //AJAX
+
+   $('.loader').fadeOut(2000, function () {
+      $('.loader').removeClass('gorunur')
+   }
+   );
+
+
+
+   //AJAX   
+
+   //ButtonClickTextShow
+
+   let prevButton = $("#owl-top .owl-prev");
+   let nextButton = $("#owl-top .owl-next");
+   let h1Text = $('.normalh1')
+
+   prevButton.on('click', function () {
+      h1Text.removeClass('up-h1');
+      setTimeout(() => {
+         h1Text.addClass('up-h1');
+      }, 250);
+
+   });
+
+   nextButton.on('click', function () {
+      h1Text.removeClass('up-h1');
+      setTimeout(() => {
+         h1Text.addClass('up-h1');
+      }, 250);
+
+   });
+
+   //ButtonClickTextShow
+
+
+   let navbar = $('.real-nav');
+
+
+   //OwlCarousel2
+
+
+   $(window).on('scroll', function () {
+      let diff = $(window).scrollTop();
+      // let navbarDiff = $(navbar).offsetTop();
+      if (diff >= 170) {
+         $(navbar).addClass('nav-scroll');
+         $('.query-nav').addClass('nav-scroll');
+      }
+
+      else {
+         $(navbar).removeClass('nav-scroll');
+         $('.query-nav').removeClass('nav-scroll');
+      }
+   })
+
+   $('.query-i button').on('click', () => {
+
+      if ($('.query-nav-click').hasClass('query-nav-block')) {
+         $('.query-nav-click').removeClass('query-nav-block');
+         $('.query-nav-click').addClass('query-nav-none');
+      }
+      else {
+         $('.query-nav-click').removeClass('query-nav-none');
+         $('.query-nav-click').addClass('query-nav-block');
+      }
+
+   })
+
+   $(window).on('scroll', function () {
+      let scrollTop = $('.scroll-top');
+      let diff = $(window).scrollTop();
+
+      if (diff >= 100) {
+         scrollTop.addClass('show');
+      }
+      else {
+         scrollTop.removeClass('show');
+      }
+   })
+
+   //THIS
+   let queryA = $('.query-a');
+
+   queryA.on('click', function () {
+
+      let navbarX = $(this).siblings();
+      let result = navbarX.hasClass('navbar-collapse-block')
+
+      $(queryA).siblings().removeClass('navbar-collapse-block');
+      $(queryA).removeClass('navbarx-collapse-blue');
+
+      if (result) {
+         $(this).removeClass('navbarx-collapse-blue');
+         $(navbarX).removeClass('navbar-collapse-block');
+      }
+      else {
+         $(this).addClass('navbarx-collapse-blue');
+         $(navbarX).addClass('navbar-collapse-block');
+      }
+
+   })
+
+
+   //THIS
+
+
+
+});
